e92f08b344e06f609bf51796c53d843fe397bfbe
Tim Noise <noizhax@gmail.com>
2013-01-04 17:45:01 +1100

    WIP

--- code/models/EveMemberCharacterCache.php
+++ code/models/EveMemberCharacterCache.php
@@ -4,11 +4,20 @@ class EveMemberCharacterCache extends DataObject
 {
     static $db = array(
         'CharacterID'   => 'Int',
-        'CharacterName' =>  'Varchar(255)'
+        'CharacterName' => 'Varchar(255)',
+        'APIHash'       => 'Varchar(32)'
     );
 
     static $has_one = array(
         'Member'    => 'Member',
         'EveApi'    => 'EveApi'
     );
+
+    function Character()
+    {
+        if($api = $this->EveApi()) {
+            return new EveCharacter($this->CharacterID, $api);
+        }
+        return false;
+    }
 }
--- code/pages/EveForumMemberPage.php
+++ code/pages/EveForumMemberPage.php
@@ -2,10 +2,23 @@
 
 class EveForumMemberPage extends EveCharacterPage
 {
-
 }
 
 class EveForumMemberPage_controller extends EveCharacterPage_controller
 {
+    function index()
+    {
+        if(!Permission::check('EVE_ROSTER')) return $this->httpError(403);
+        return $this;
+    }
 
+    function CharactersFromCache()
+    {
+        return EveMemberCharacterCache::get('EveMemberCharacterCache');
+    }
+
+    function MembersInCorp()
+    {
+        return Group::get_by_code('rioters')->Members();
+    }
 }
